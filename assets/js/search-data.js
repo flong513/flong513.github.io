d485df187ce1bae8ebf0ac3c6a36a62671f4f21f
flong513 <65579432+flong513@users.noreply.github.com>
2025-03-09 00:15:16 +0000

    Deploying to gh-pages from @ flong513/flong513.github.io@609542742c54958d0b9ba464d797d0b171ec11a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,12 +51,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
+        },{id: "nav-",
+          title: "",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/_pages/profiles/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
