771af9401eaf20b38ced87743e83f46793300941
flong513 <65579432+flong513@users.noreply.github.com>
2025-08-24 19:01:36 +0000

    Deploying to gh-pages from @ flong513/flong513.github.io@0d645eae1e78a01424fbbf0fe8e39e83ef61a862 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "coming soon!",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
